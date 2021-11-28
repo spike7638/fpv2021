8abcf68437e401b5f63e6e8fc8042edf88ca83c4
John_Hughes@brown.edu <11300898+spike7638@users.noreply.github.com>
2021-11-28 17:18:31 -0500

    more

--- src/lectures/tinyDemo.lean
+++ src/lectures/tinyDemo.lean
@@ -28,9 +28,26 @@ namespace monolithic_group
 (mul_left_inv : ∀a, mul (inv a) a = one)
 
 #print group
+
+-- I'd like to say "for the next page or so, G denotes agroup." That seems to be 
+-- what the next two lines do.
+
 universe u
 variables {G : Type } [group G]
+-- The [group G] would SEEM to suggest that G is a group, but I can't find anywhere in the manual that explains 
+-- this syntax
 
-lemma mul_assoc : ∀ a b c : G, mul (mul a b) c = mul a (mul b c)) := sorry
+-- Now I'd like to show that multiplication in a group is associative, presumably using the "mul_assoc" part 
+-- of the definintion of a group. 
+
+-- I copied this lemma from mathlib/src/algebra/group/defs.lean, where it's written using an infix "*"
+-- instead of "mul", so I swapped in the "mul" form. Unfortunately, "mul" seems to be an unknown identifier, 
+-- even though a,b,c are of type G, where G is (I thought) a group. 
+
+-- lemma mul_assoc : ∀ a b c:G , mul (mul a b) c = mul a (mul b c)) := sorry
+
+lemma mul_assoc : ∀ a b c : G, group.mul (group.mul a b) c = group.mul a (group.mul b c) := group.mul_assoc
 
 end monolithic_group
+
+end LoVe
